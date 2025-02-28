394b6b971ae7e18792c8ebaf49a38804f9aa2638
Mark Alexandrovich <maxwebgt@vetklinika54.ru>
2025-03-01 02:10:55 +0700

    удалил мусор, и продолжаем

--- check-docker-compose.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-# This file should not be used - functionality is integrated directly in docker-compose.yml
--- jenkins/jenkins-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Fix permissions
-mkdir -p /var/jenkins_home/workspace/Projects
-chown -R jenkins:jenkins /var/jenkins_home
-chmod -R 777 /var/jenkins_home/workspace
-
-# Start Jenkins
-exec /usr/local/bin/jenkins.sh
